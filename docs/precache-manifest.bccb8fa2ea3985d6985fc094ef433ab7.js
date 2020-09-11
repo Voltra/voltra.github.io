fac9657fc6da98ee88a4be0015df3c72b80f96cf
Voltra <ludwig.guerin.98@gmx.fr>
2020-09-11 13:21:00 +0200

    -> v1.2.1

---
+++ docs/precache-manifest.bccb8fa2ea3985d6985fc094ef433ab7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.nojekyll"
+  },
+  {
+    "revision": "22fb7019ddbaba18cac85237364b89e3",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "3d5e3029438fde4a3edc",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "014e36e0b97341cf223f",
+    "url": "/css/chunk-136097bf.css"
+  },
+  {
+    "revision": "f18a388ba4d13f0d5b70",
+    "url": "/css/chunk-14eed3ac.css"
+  },
+  {
+    "revision": "2f82abb69e463655aa3e",
+    "url": "/css/chunk-6461f60b.css"
+  },
+  {
+    "revision": "0c10f284d3410e38da90",
+    "url": "/css/chunk-68d61c20.css"
+  },
+  {
+    "revision": "6685d50efc45d47af746",
+    "url": "/css/chunk-893b923e.css"
+  },
+  {
+    "revision": "fdccad8425cd6f15606e0e80be86d22c",
+    "url": "/img/bkp/ludwig.png"
+  },
+  {
+    "revision": "a97104bd80f65684b6a11685947adac2",
+    "url": "/img/bkp/og.png"
+  },
+  {
+    "revision": "d0b811c6adadd5c696e9d7bfd0d2b328",
+    "url": "/img/ludwig.jpg"
+  },
+  {
+    "revision": "d3ed8fe317956606c80a3bcd2d3b8b92",
+    "url": "/img/og.jpg"
+  },
+  {
+    "revision": "98d30409fef150fe772a73decfde32bd",
+    "url": "/img/voltra.png"
+  },
+  {
+    "revision": "e87c501afe2af8912790aa99f316cd2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5e3029438fde4a3edc",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "014e36e0b97341cf223f",
+    "url": "/js/chunk-136097bf.js"
+  },
+  {
+    "revision": "f18a388ba4d13f0d5b70",
+    "url": "/js/chunk-14eed3ac.js"
+  },
+  {
+    "revision": "7892676e93e841787892",
+    "url": "/js/chunk-2d0e95df.js"
+  },
+  {
+    "revision": "2f82abb69e463655aa3e",
+    "url": "/js/chunk-6461f60b.js"
+  },
+  {
+    "revision": "0c10f284d3410e38da90",
+    "url": "/js/chunk-68d61c20.js"
+  },
+  {
+    "revision": "6685d50efc45d47af746",
+    "url": "/js/chunk-893b923e.js"
+  },
+  {
+    "revision": "8364b83e8c2cedb90a4b",
+    "url": "/js/chunk-vendors.js"
+  },
+  {
+    "revision": "2668ccba6b48e768f9a33b3b3ff2a3a1",
+    "url": "/manifest.json"
+  }
+]);
